2e4febebb61b33e1373a79a19d056960fbb16fc4
AnneCleres28 <annecleres.ac@gmail.com>
2022-08-02 20:41:32 -0300

    Second Exemplo

---
+++ src/curso/dio/heranca/Exemplo2/ClasseFilha1.java
@@ -0,0 +1,10 @@
+package curso.dio.heranca.Exemplo2;
+
+public class ClasseFilha1 extends ClasseMae {
+
+    @Override
+    void metodo1() {
+        System.out.println("Método 1 da Classe Filha 1");
+    }
+
+}
